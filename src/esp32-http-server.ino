8c77d542f08177afbf7484d412dffc7ff0ab156c
Yassin-Zaher <yassinzaherelfeliri@gmail.com>
2025-05-09 14:16:05 +0100

    initial commit

--- src/esp32-http-server.ino
+++ src/esp32-http-server.ino
@@ -1,109 +1,294 @@
-/* ESP32 HTTP IoT Server Example for Wokwi.com
-
-  https://wokwi.com/arduino/projects/320964045035274834
-
-  When running it on Wokwi for VSCode, you can connect to the 
-  simulated ESP32 server by opening http://localhost:8180
-  in your browser. This is configured by wokwi.toml.
-*/
-
+#include <Adafruit_Sensor.h>
+#include <DHT_U.h>
 #include <WiFi.h>
-#include <WiFiClient.h>
-#include <WebServer.h>
-#include <uri/UriBraces.h>
+#include <PubSubClient.h>
+#include <ESP32Servo.h> // Include the Servo library
+#include <FastLED.h>    // Include the FastLED library
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-#define WIFI_SSID "Wokwi-GUEST"
-#define WIFI_PASSWORD ""
-// Defining the WiFi channel speeds up the connection:
-#define WIFI_CHANNEL 6
+// Defining Pins
+#define DHTPIN 12
+#define LED 26
+#define SERVO_PIN 2 // Servo motor pin
+#define LED_PIN 4   // WS2812 LED strip pin
+#define NUM_LEDS 16 // Number of LEDs in the strip
 
-WebServer server(80);
+// PIR sensor initial cong=fig
+int ledPinPIR = 25;    // choose the pin for the LED
+int inputPinPIR = 14;  // choose the input pin (for PIR sensor)
+int pirStatePIR = LOW; // we start, assuming no motion detected
+int valPIR = 0;        // variable for reading the pin status
 
-const int LED1 = 26;
-const int LED2 = 27;
+// Pins for potentiometer
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1 // No reset pin
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+const int potPin = 35;
 
-bool led1State = false;
-bool led2State = false;
+// DHT parameters
+#define DHTTYPE DHT22 // DHT 11
+DHT_Unified dht(DHTPIN, DHTTYPE);
+uint32_t delayMS;
 
-void sendHtml() {
-  String response = R"(
-    <!DOCTYPE html><html>
-      <head>
-        <title>ESP32 Web Server Demo</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style>
-          html { font-family: sans-serif; text-align: center; }
-          body { display: inline-flex; flex-direction: column; }
-          h1 { margin-bottom: 1.2em; } 
-          h2 { margin: 0; }
-          div { display: grid; grid-template-columns: 1fr 1fr; grid-template-rows: auto auto; grid-auto-flow: column; grid-gap: 1em; }
-          .btn { background-color: #5B5; border: none; color: #fff; padding: 0.5em 1em;
-                 font-size: 2em; text-decoration: none }
-          .btn.OFF { background-color: #333; }
-        </style>
-      </head>
-            
-      <body>
-        <h1>ESP32 Web Server</h1>
+// Servo motor
+Servo servo;
 
-        <div>
-          <h2>LED 1</h2>
-          <a href="/toggle/1" class="btn LED1_TEXT">LED1_TEXT</a>
-          <h2>LED 2</h2>
-          <a href="/toggle/2" class="btn LED2_TEXT">LED2_TEXT</a>
-        </div>
-      </body>
-    </html>
-  )";
-  response.replace("LED1_TEXT", led1State ? "ON" : "OFF");
-  response.replace("LED2_TEXT", led2State ? "ON" : "OFF");
-  server.send(200, "text/html", response);
-}
+// WS2812 LED strip
+CRGB leds[NUM_LEDS];
 
-void setup(void) {
-  Serial.begin(115200);
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
+// MQTT Credentials
+const char *ssid = "Wokwi-GUEST"; // Setting your AP SSID
+const char *password = "";        // Setting your AP PSK
+const char *mqttServer = "broker.hivemq.com";
+// const char* mqttUserName = "bqzbdodo";
+// const char* mqttPwd = "5oU2W_QN2WD8";
+const char *clientID = "ujaisldaaasdfgh;laslksdja1"; // Client ID username+0001
+const char *topic = "Tempdata";                      // Publish topic
 
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD, WIFI_CHANNEL);
-  Serial.print("Connecting to WiFi ");
-  Serial.print(WIFI_SSID);
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(100);
+// Parameters for using non-blocking delay
+unsigned long previousMillis = 0;
+const long interval = 1000;
+String msgStr = ""; // MQTT message buffer
+float temp, hum;
+
+// Setting up WiFi and MQTT client
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi()
+{
+  delay(10);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
     Serial.print(".");
   }
-  Serial.println(" Connected!");
-
-  Serial.print("IP address: ");
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-
-  server.on("/", sendHtml);
-
-  server.on(UriBraces("/toggle/{}"), []() {
-    String led = server.pathArg(0);
-    Serial.print("Toggle LED #");
-    Serial.println(led);
-
-    switch (led.toInt()) {
-      case 1:
-        led1State = !led1State;
-        digitalWrite(LED1, led1State);
-        break;
-      case 2:
-        led2State = !led2State;
-        digitalWrite(LED2, led2State);
-        break;
-    }
-
-    sendHtml();
-  });
-
-  server.begin();
-  Serial.println("HTTP server started (http://localhost:8180)");
 }
 
-void loop(void) {
-  server.handleClient();
-  delay(2);
+void reconnect()
+{
+  while (!client.connected())
+  {
+    if (client.connect(clientID))
+    {
+      Serial.println("MQTT connected");
+      client.subscribe("lights");
+      client.subscribe("servo");           // Subscribe to servo topic
+      client.subscribe("lights/neopixel"); // Subscribe to neopixel topic
+      Serial.println("Topic Subscribed");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000); // wait 5sec and retry
+    }
+  }
+}
+
+// Subscribe callback
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.print("Message: ");
+  String data = "";
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+    data += (char)payload[i];
+  }
+  Serial.println();
+  Serial.print("Message size: ");
+  Serial.println(length);
+  Serial.println();
+  Serial.println("-----------------------");
+  Serial.println(data);
+
+  if (String(topic) == "lights")
+  {
+    if (data == "ON")
+    {
+      Serial.println("LED");
+      digitalWrite(LED, HIGH);
+    }
+    else
+    {
+      digitalWrite(LED, LOW);
+    }
+  }
+  else if (String(topic) == "servo")
+  {
+    int degree = data.toInt(); // Convert the received data to an integer
+    Serial.print("Moving servo to degree: ");
+    Serial.println(degree);
+    servo.write(degree); // Move the servo to the specified degree
+  }
+  else if (String(topic) == "lights/neopixel")
+  {
+    int red, green, blue;
+    sscanf(data.c_str(), "%d,%d,%d", &red, &green, &blue); // Parse the received data into RGB values
+    Serial.print("Setting NeoPixel color to (R,G,B): ");
+    Serial.print(red);
+    Serial.print(",");
+    Serial.print(green);
+    Serial.print(",");
+    Serial.println(blue);
+    fill_solid(leds, NUM_LEDS, CRGB(red, green, blue)); // Set all LEDs in the strip to the specified color
+    FastLED.show();                                     // Update the LED strip with the new color
+    fill_solid(leds, NUM_LEDS, CRGB(red, green, blue));
+    FastLED.show();
+  }
+}
+
+void handlePIRSensor()
+{
+  valPIR = digitalRead(inputPinPIR); // read input value
+
+  if (valPIR == HIGH)
+  {                                // check if the input is HIGH
+    digitalWrite(ledPinPIR, HIGH); // turn LED ON
+    if (pirStatePIR == LOW)
+    {
+      Serial.println("Motion detected!");
+      pirStatePIR = HIGH;
+    }
+  }
+  else
+  {
+    digitalWrite(ledPinPIR, LOW); // turn LED OFF
+    if (pirStatePIR == HIGH)
+    {
+      Serial.println("Motion ended!");
+      pirStatePIR = LOW;
+    }
+  }
+}
+
+// set up the OLED for potentiometer
+void initOLED()
+{
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  {
+    Serial.println("OLED init failed");
+    while (1)
+      ; // Stop execution if OLED not found
+  }
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println("OLED Ready");
+  display.display();
+}
+
+void displayPotValue(int potValue)
+{
+  float voltage = potValue * (3.3 / 4095.0);
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print("Pot:");
+  display.println(potValue);
+
+  display.print("Volt:");
+  display.print(voltage, 2);
+  display.display();
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  // Initialize device.
+  dht.begin();
+  // Get temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  dht.humidity().getSensor(&sensor);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+
+  // Setup servo
+  servo.attach(SERVO_PIN, 500, 2400);
+  servo.write(0);
+
+  // Setup WS2812 LED strip
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+
+  setup_wifi();
+  client.setServer(mqttServer, 1883); // Setting MQTT server
+  client.setCallback(callback);       // Define function which will be called when a message is received.
+
+  // PIR SENSOR INITIAL SETUP
+  pinMode(ledPinPIR, OUTPUT);  // declare LED as output
+  pinMode(inputPinPIR, INPUT); // declare sensor as input
+
+  // Initialize potentiometer
+  initOLED();
+}
+
+void loop()
+{
+
+  handlePIRSensor(); // Call the PIR handler
+  if (!client.connected())
+  {              // If client is not connected
+    reconnect(); // Try to reconnect
+  }
+  client.loop();
+  unsigned long currentMillis = millis(); // Read current time
+  if (currentMillis - previousMillis >= interval)
+  { // If current time - last time > 5 sec
+    previousMillis = currentMillis;
+    // Read temperature and humidity
+    sensors_event_t event;
+    dht.temperature().getEvent(&event);
+    if (isnan(event.temperature))
+    {
+      Serial.println(F("Error reading temperature!"));
+    }
+    else
+    {
+      Serial.print(F("Temperature: "));
+      temp = event.temperature;
+      Serial.print(temp);
+      Serial.println(F("°C"));
+    }
+    // Get humidity event and print its value
+    dht.humidity().getEvent(&event);
+    if (isnan(event.relative_humidity))
+    {
+      Serial.println(F("Error reading humidity!"));
+    }
+    else
+    {
+      Serial.print(F("Humidity: "));
+      hum = event.relative_humidity;
+      Serial.print(hum);
+      Serial.println(F("%"));
+    }
+    msgStr = String(temp) + "," + String(hum) + ",";
+    byte arrSize = msgStr.length() + 1;
+    char msg[arrSize];
+    Serial.print("PUBLISH DATA: ");
+    Serial.println(msgStr);
+    msgStr.toCharArray(msg, arrSize);
+    client.publish(topic, msg);
+    msgStr = "";
+    delay(1);
+  }
+
+  int value = analogRead(potPin);
+  displayPotValue(value);
+  delay(500); // Only update twice per second
 }
